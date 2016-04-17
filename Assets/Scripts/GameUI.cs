efd301a20adb7483ede2054448929e12b8aab797
aytona <christopher.aytona@gmail.com>
2016-04-17 01:34:44 -0400

    In-game UI added

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -3,5 +3,58 @@ using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
-    	
+    public GameObject[] scenesArray;
+
+    private bool inPause = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && !inPause)
+        {
+            PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        ResetSceneArray();
+        ActivateScene("Main");
+        inPause = false;
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void PauseGame()
+    {
+        inPause = true;
+        Time.timeScale = 0;
+        ResetSceneArray();
+        ActivateScene("Pause");
+    }
+
+    // TODO: Put resetscenearray() and activatescene() into a different class
+    private void ResetSceneArray()
+    {
+        foreach (GameObject i in scenesArray)
+        {
+            if (i.activeInHierarchy)
+            {
+                i.SetActive(false);
+            }
+        }
+    }
+
+    private void ActivateScene(string name)
+    {
+        foreach (GameObject i in scenesArray)
+        {
+            if (i.name == name)
+            {
+                i.SetActive(true);
+            }
+        }
+    }
 }
