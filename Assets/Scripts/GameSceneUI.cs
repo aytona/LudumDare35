c1dd8a2ed4a22c13df64ba1df867ea71b30d5692
aytona <christopher.aytona@gmail.com>
2016-06-08 23:06:49 -0400

    Menu script, ShapesScene function

---
+++ Assets/Scripts/GameSceneUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSceneUI : MonoBehaviour {
+
+	public void PlayShapesScene() {
+		Application.LoadLevel("ShapeScene");
+	}
+
+	public void QuitApp() {
+		Application.Quit();
+	}
+}
