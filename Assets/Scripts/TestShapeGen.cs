f92ebe68d87521793b8840e96377be840ab3f8d4
aytona <christopher.aytona@gmail.com>
2016-06-10 03:42:53 -0400

    Grid generation test

--- Assets/Scripts/TestShapeGen.cs
+++ Assets/Scripts/TestShapeGen.cs
@@ -7,6 +7,9 @@ public class TestShapeGen : MonoBehaviour {
 	public int xSize, ySize;
 
 	private Vector3[] vertices;
+	private Vector2[] uv;
+	private int[] triangles;
+	private Mesh mesh;
 
 	void Awake() {
 		GenerateShape();
@@ -24,13 +27,31 @@ public class TestShapeGen : MonoBehaviour {
 	}
 
 	private void GenerateShape() {
+		mesh = GetComponent<MeshFilter>().mesh;
+		mesh.name = "Grid";
+
 		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		uv = new Vector2[vertices.Length];
 		int i = 0;
 		for (int y = 0; y <= ySize; y++) {
 			for (int x = 0; x <= xSize; x++) {
 				vertices[i] = new Vector3(x, y);
+				uv[i] = new Vector2(x/xSize, y/ySize);
 				i++;
 			}
 		}
+		mesh.vertices = vertices;
+		mesh.uv = uv;
+
+		triangles = new int[xSize * ySize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+			}
+		}
+		mesh.triangles = triangles;
 	}
 }
