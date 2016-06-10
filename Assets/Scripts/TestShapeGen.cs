40e46bbb0dae1afac9d1da5751b9f3858269e731
aytona <christopher.aytona@gmail.com>
2016-06-09 23:54:31 -0400

    Grid test

---
+++ Assets/Scripts/TestShapeGen.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class TestShapeGen : MonoBehaviour {
+
+	public int xSize, ySize;
+
+	private Vector3[] vertices;
+
+	void Awake() {
+		GenerateShape();
+	}
+
+	void OnDrawGizmos() {
+		if (vertices == null) {
+			return;
+		}
+
+		Gizmos.color = Color.black;
+		for (int i = 0; i < vertices.Length; i++) {
+			Gizmos.DrawSphere(vertices[i], 0.1f);
+		}
+	}
+
+	private void GenerateShape() {
+		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		int i = 0;
+		for (int y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++) {
+				vertices[i] = new Vector3(x, y);
+				i++;
+			}
+		}
+	}
+}
