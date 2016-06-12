6e07715ed9bd6b74113d2c8e7ae6c6a01b50e9ee
Christopher Aytona <christopher.aytona@gmail.com>
2016-06-11 23:53:01 -0400

    initial matching

---
+++ Assets/Scripts/Matching.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Matching : MonoBehaviour {
+
+	// Simple matching
+
+	public string[] ShapeNames;
+
+	private string requiredShapeName;
+	private bool waitingForMatch = true;
+
+	void Update() {
+		if (waitingForMatch) {
+			PickRandomShape();
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+
+		}
+	}
+
+	private void PickRandomShape() {
+		requiredShapeName = ShapeNames [Random.Range(0, ShapeNames.Length)];
+		waitingForMatch = false;
+	}
+}
