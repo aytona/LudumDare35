b181479960260359facf4fd7d3333420f4cb3dc1
aytona <christopher.aytona@gmail.com>
2016-08-17 23:05:59 -0600

    Initial menu UI

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Aytona.UI
+{
+    class MenuUI : MonoBehaviour
+    {
+        public GameObject[] canvases;
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void SwitchCanvas(string target)
+        {
+            foreach(GameObject canvas in canvases)
+            {
+                if (canvas.name != target)
+                {
+                    canvas.SetActive(false);
+                }
+                else
+                {
+                    canvas.SetActive(true);
+                }
+            }
+        }
+    }
+}
