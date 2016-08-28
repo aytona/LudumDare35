7d5e36dde1949af28d09629f65641b0f371cb6c2
aytona <christopher.aytona@gmail.com>
2016-08-28 04:13:19 -0400

    Sound fx added

---
+++ Assets/Scripts/OnClickSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Aytona.Effect
+{
+    public class OnClickSound : MonoBehaviour
+    {
+        public GameObject audioObj;
+        public AudioClip clip;
+
+        public void PlayClip()
+        {
+            AudioSource audio = audioObj.GetComponent<AudioSource>();
+            audio.clip = clip;
+            audio.Play();
+        }
+    }
+}
