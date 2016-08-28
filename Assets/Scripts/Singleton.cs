7efc376620bcddc1c9f69a4c0d9e4eb3db49bca1
aytona <christopher.aytona@gmail.com>
2016-08-28 04:12:05 -0400

    Singleton script added

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Aytona.Global
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        public static T _Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject singleton = new GameObject(typeof(T).Name);
+                        _instance = singleton.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
