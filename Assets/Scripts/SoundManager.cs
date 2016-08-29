ba5be58558989863e8bbef18223e1b370e28ee53
aytona <christopher.aytona@gmail.com>
2016-08-29 00:04:11 -0400

    Sound manager added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace Aytona.Sound
+{
+    public class SoundManager : Global.Singleton<SoundManager>
+    {
+        public List<AudioSource> SFXSources;
+
+        void Start()
+        {
+            foreach(AudioSource source in gameObject.GetComponentsInChildren<AudioSource>(true))
+            {
+                SFXSources.Add(source);
+            }
+        }
+
+        public void UpdateAudioVolume(Slider slider)
+        {
+            foreach(AudioSource source in SFXSources)
+            {
+                source.volume = slider.value;
+            }
+        }
+    }
+}
