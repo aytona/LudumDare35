69c2f6b2b72084f5b3438386f4deba0f2ce2d88b
aytona <christopher.aytona@gmail.com>
2016-09-02 00:03:54 -0400

    A small cursor anim on click script

---
+++ Assets/Scripts/CursorAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Aytona.UI
+{
+    public class CursorAnim : Global.Singleton<CursorAnim>
+    {
+        public Texture2D cursorImg;
+        private CursorMode cursorMode = CursorMode.Auto;
+        private Vector2 downPressAnim = Vector2.down * 5f;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Cursor.SetCursor(cursorImg, downPressAnim, cursorMode);
+            }
+
+            else if (Input.GetMouseButtonUp(0))
+            {
+                Cursor.SetCursor(cursorImg, Vector2.zero, cursorMode);
+            }
+        }
+    }
+}
